5925f4b50a69dcf56079134678ffe546154d901e
Prateek <jdev.prateek@gmail.com>
2026-01-22 16:55:19 +0530

    init

---
+++ src/test/java/com/ps/ecom/config/server/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ps.ecom.config.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
