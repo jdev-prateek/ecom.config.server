abd3a525d7853aefa43b803bd46b3fd2d36d914c
Prateek <jdev.prateek@gmail.com>
2026-01-30 09:08:58 +0530

    init

--- src/main/java/com/ps/ecom/config/server/ConfigServerApplication.java
+++ src/main/java/com/ps/ecom/config/server/ConfigServerApplication.java
@@ -8,7 +8,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 @EnableConfigServer
 public class ConfigServerApplication {
     // 1
-    // 2.4
+    // 2.5
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigServerApplication.class, args);
 	}
